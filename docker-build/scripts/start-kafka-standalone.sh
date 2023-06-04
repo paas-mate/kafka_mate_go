142aef83e8c1f56b5faffa64a7b1f79fe87e203a
goflutterjava <goflutterjava@gmail.com>
2023-06-04 17:27:22 +0800

    external zk when standalone kafka

--- docker-build/scripts/start-kafka-standalone.sh
+++ docker-build/scripts/start-kafka-standalone.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-nohup $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties >>$KAFKA_HOME/logs/zookeeper.stdout.log 2>>$KAFKA_HOME/logs/zookeeper.stderr.log &
+if [ ${ZK_ADDR} ];then
+    sed -i s/localhost:2181/${ZK_ADDR}/g $KAFKA_HOME/config/server.properties
+else
+    nohup $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties >>$KAFKA_HOME/logs/zookeeper.stdout.log 2>>$KAFKA_HOME/logs/zookeeper.stderr.log &
+fi
 nohup $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties >>$KAFKA_HOME/logs/kafka.stdout.log 2>>$KAFKA_HOME/logs/kafka.stderr.log &
